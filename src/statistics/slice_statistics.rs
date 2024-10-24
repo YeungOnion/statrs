YeungOnion/statrs

commit a3f78fe554f18715ccc4ea906ff537a32dff1ad0

doc: ensure tests use new traits
doc: ensure tests use new traits in `Categorical`

doc: ensure tests use new traits in `LogNormal`

doc: ensure tests use new traits in `Poisson`

doc: ensure tests use new traits in `Bernoulli` and `Binomial`

doc: ensure tests use new traits

src/statistics/slice_statistics.rs
@@ -317,13 +317,13 @@ impl<D: AsMut<[f64]> + AsRef<[f64]>> StandardizedMoment<f64> for Data<D> {
     /// #[macro_use]
     /// extern crate statrs;
     ///
-    /// use statrs::statistics::Distribution;
+    /// use statrs::statistics::StandardizedMoment;
     /// use statrs::statistics::Data;
     ///
     /// # fn main() {
     /// let x = [];
     /// let x = Data::new(x);
-    /// assert!(x.mean().unwrap().is_nan());
+    /// assert!(x.mean().is_none());
     ///
     /// let y = [0.0, f64::NAN, 3.0, -2.0];
     /// let y = Data::new(y);
@@ -362,12 +362,12 @@ impl<D: AsMut<[f64]> + AsRef<[f64]>> StandardizedMoment<f64> for Data<D> {
     /// # Examples
     ///
     /// ```
-    /// use statrs::statistics::Distribution;
+    /// use statrs::statistics::StandardizedMoment;
     /// use statrs::statistics::Data;
     ///
     /// let x = [];
     /// let x = Data::new(x);
-    /// assert!(x.variance().unwrap().is_nan());
+    /// assert!(x.variance().is_none());
     ///
     /// let y = [0.0, f64::NAN, 3.0, -2.0];
     /// let y = Data::new(y);
@@ -378,19 +378,18 @@ impl<D: AsMut<[f64]> + AsRef<[f64]>> StandardizedMoment<f64> for Data<D> {
     /// assert_eq!(z.variance().unwrap(), 19.0 / 3.0);
     /// ```
     fn variance(&self) -> Option<f64> {
-        if self.0.as_ref().is_empty() {
-            return None;
-        }
+        // TODO: rewrite iterator statistics and rely on them instead
+        let mut iter = self.0.as_ref().iter().enumerate();
 
-        let mut sum = 0.0;
+        let mut sum = *iter.next()?.1;
         let mut variance = 0.0;
 
-        for (i, &x) in self.0.as_ref().iter().enumerate() {
+        for (i, &x) in iter {
             if x.is_nan() {
                 return Some(f64::NAN);
             }
-            sum += x;
             let diff = (i + 1) as f64 * x - sum;
+            sum += x;
             variance += diff * diff / (i * (i + 1)) as f64
         }
         Some(variance / (self.0.as_ref().len() - 1) as f64)

src/distribution/bernoulli.rs
@@ -10,13 +10,14 @@ use crate::statistics::*;
 /// # Examples
 ///
 /// ```
-/// use statrs::distribution::{Bernoulli, Discrete};
-/// use statrs::statistics::Distribution;
+/// use statrs::distribution::{Bernoulli, BinomialError, Discrete};
+/// use statrs::statistics::*;
 ///
-/// let n = Bernoulli::new(0.5).unwrap();
-/// assert_eq!(n.mean().unwrap(), 0.5);
+/// let n = Bernoulli::new(0.5)?;
+/// assert_eq!(n.mean(), 0.5);
 /// assert_eq!(n.pmf(0), 0.5);
 /// assert_eq!(n.pmf(1), 0.5);
+/// # Ok::<(), BinomialError>(())
 /// ```
 #[derive(Copy, Clone, PartialEq, Debug)]
 pub struct Bernoulli {
@@ -53,10 +54,11 @@ impl Bernoulli {
     /// # Examples
     ///
     /// ```
-    /// use statrs::distribution::Bernoulli;
+    /// use statrs::distribution::{Bernoulli, BinomialError};
     ///
-    /// let n = Bernoulli::new(0.5).unwrap();
+    /// let n = Bernoulli::new(0.5)?;
     /// assert_eq!(n.p(), 0.5);
+    /// # Ok::<(), BinomialError>(())
     /// ```
     pub fn p(&self) -> f64 {
         self.b.p()
@@ -68,10 +70,11 @@ impl Bernoulli {
     /// # Examples
     ///
     /// ```
-    /// use statrs::distribution::Bernoulli;
+    /// use statrs::distribution::{Bernoulli, BinomialError};
     ///
-    /// let n = Bernoulli::new(0.5).unwrap();
+    /// let n = Bernoulli::new(0.5)?;
     /// assert_eq!(n.n(), 1);
+    /// # Ok::<(), BinomialError>(())
     /// ```
     pub fn n(&self) -> u64 {
         1

src/distribution/beta.rs
@@ -8,13 +8,14 @@ use crate::statistics::*;
 /// # Examples
 ///
 /// ```
-/// use statrs::distribution::{Beta, Continuous};
+/// use statrs::distribution::{Beta, Continuous, BetaError};
 /// use statrs::statistics::*;
 /// use statrs::prec;
 ///
-/// let n = Beta::new(2.0, 2.0).unwrap();
-/// assert_eq!(n.mean().unwrap(), 0.5);
+/// let n = Beta::new(2.0, 2.0)?;
+/// assert_eq!(n.mean(), 0.5);
 /// assert!(prec::almost_eq(n.pdf(0.5), 1.5, 1e-14));
+/// # Ok::<(), BetaError>(())
 /// ```
 #[derive(Copy, Clone, PartialEq, Debug)]
 pub struct Beta {
@@ -82,10 +83,11 @@ impl Beta {
     /// # Examples
     ///
     /// ```
-    /// use statrs::distribution::Beta;
+    /// use statrs::distribution::{Beta, BetaError};
     ///
-    /// let n = Beta::new(1.0, 2.0).unwrap();
+    /// let n = Beta::new(1.0, 2.0)?;
     /// assert_eq!(n.shape_a(), 1.0);
+    /// # Ok::<(), BetaError>(())
     /// ```
     pub fn shape_a(&self) -> f64 {
         self.shape_a
@@ -96,10 +98,11 @@ impl Beta {
     /// # Examples
     ///
     /// ```
-    /// use statrs::distribution::Beta;
+    /// use statrs::distribution::{Beta, BetaError};
     ///
-    /// let n = Beta::new(1.0, 2.0).unwrap();
+    /// let n = Beta::new(1.0, 2.0)?;
     /// assert_eq!(n.shape_b(), 2.0);
+    /// # Ok::<(), BetaError>(())
     /// ```
     pub fn shape_b(&self) -> f64 {
         self.shape_b

src/distribution/binomial.rs
@@ -10,13 +10,14 @@ use std::f64;
 /// # Examples
 ///
 /// ```
-/// use statrs::distribution::{Binomial, Discrete};
-/// use statrs::statistics::Distribution;
+/// use statrs::distribution::{Binomial, BinomialError, Discrete};
+/// use statrs::statistics::*;
 ///
-/// let n = Binomial::new(0.5, 5).unwrap();
-/// assert_eq!(n.mean().unwrap(), 2.5);
+/// let n = Binomial::new(0.5, 5)?;
+/// assert_eq!(n.mean(), 2.5);
 /// assert_eq!(n.pmf(0), 0.03125);
 /// assert_eq!(n.pmf(3), 0.3125);
+/// # Ok::<(), BinomialError>(())
 /// ```
 #[derive(Copy, Clone, PartialEq, Debug)]
 pub struct Binomial {
@@ -78,10 +79,11 @@ impl Binomial {
     /// # Examples
     ///
     /// ```
-    /// use statrs::distribution::Binomial;
+    /// use statrs::distribution::{Binomial, BinomialError};
     ///
-    /// let n = Binomial::new(0.5, 5).unwrap();
+    /// let n = Binomial::new(0.5, 5)?;
     /// assert_eq!(n.p(), 0.5);
+    /// # Ok::<(), BinomialError>(())
     /// ```
     pub fn p(&self) -> f64 {
         self.p
@@ -93,10 +95,11 @@ impl Binomial {
     /// # Examples
     ///
     /// ```
-    /// use statrs::distribution::Binomial;
+    /// use statrs::distribution::{Binomial, BinomialError};
     ///
-    /// let n = Binomial::new(0.5, 5).unwrap();
+    /// let n = Binomial::new(0.5, 5)?;
     /// assert_eq!(n.n(), 5);
+    /// # Ok::<(), BinomialError>(())
     /// ```
     pub fn n(&self) -> u64 {
         self.n

src/distribution/categorical.rs
@@ -10,13 +10,14 @@ use std::f64;
 /// # Examples
 ///
 /// ```
-/// use statrs::distribution::{Categorical, Discrete};
-/// use statrs::statistics::Distribution;
+/// use statrs::distribution::{Categorical, CategoricalError, Discrete};
+/// use statrs::statistics::*;
 /// use statrs::prec;
 ///
-/// let n = Categorical::new(&[0.0, 1.0, 2.0]).unwrap();
-/// assert!(prec::almost_eq(n.mean().unwrap(), 5.0 / 3.0, 1e-15));
+/// let n = Categorical::new(&[0.0, 1.0, 2.0])?;
+/// assert!(prec::almost_eq(n.mean(), 5.0 / 3.0, 1e-15));
 /// assert_eq!(n.pmf(1), 1.0 / 3.0);
+/// # Ok::<(), CategoricalError>(())
 /// ```
 #[derive(Clone, PartialEq, Debug)]
 pub struct Categorical {

src/distribution/cauchy.rs
@@ -8,12 +8,13 @@ use std::f64;
 /// # Examples
 ///
 /// ```
-/// use statrs::distribution::{Cauchy, Continuous};
-/// use statrs::statistics::Mode;
+/// use statrs::distribution::{Cauchy, Continuous, CauchyError};
+/// use statrs::statistics::*;
 ///
-/// let n = Cauchy::new(0.0, 1.0).unwrap();
+/// let n = Cauchy::new(0.0, 1.0)?;
 /// assert_eq!(n.mode().unwrap(), 0.0);
 /// assert_eq!(n.pdf(1.0), 0.1591549430918953357689);
+/// # Ok::<(), CauchyError>(())
 /// ```
 #[derive(Copy, Clone, PartialEq, Debug)]
 pub struct Cauchy {
@@ -80,10 +81,11 @@ impl Cauchy {
     /// # Examples
     ///
     /// ```
-    /// use statrs::distribution::Cauchy;
+    /// use statrs::distribution::{Cauchy, CauchyError};
     ///
-    /// let n = Cauchy::new(0.0, 1.0).unwrap();
+    /// let n = Cauchy::new(0.0, 1.0)?;
     /// assert_eq!(n.location(), 0.0);
+    /// # Ok::<(), CauchyError>(())
     /// ```
     pub fn location(&self) -> f64 {
         self.location
@@ -94,10 +96,11 @@ impl Cauchy {
     /// # Examples
     ///
     /// ```
-    /// use statrs::distribution::Cauchy;
+    /// use statrs::distribution::{Cauchy, CauchyError};
     ///
-    /// let n = Cauchy::new(0.0, 1.0).unwrap();
+    /// let n = Cauchy::new(0.0, 1.0)?;
     /// assert_eq!(n.scale(), 1.0);
+    /// # Ok::<(), CauchyError>(())
     /// ```
     pub fn scale(&self) -> f64 {
         self.scale

src/distribution/chi.rs
@@ -9,13 +9,14 @@ use std::f64;
 /// # Examples
 ///
 /// ```
-/// use statrs::distribution::{Chi, Continuous};
-/// use statrs::statistics::Distribution;
+/// use statrs::distribution::{Chi, Continuous, ChiError};
+/// use statrs::statistics::*;
 /// use statrs::prec;
 ///
-/// let n = Chi::new(2.0).unwrap();
+/// let n = Chi::new(2.0)?;
 /// assert!(prec::almost_eq(n.mean().unwrap(), 1.25331413731550025121, 1e-14));
 /// assert!(prec::almost_eq(n.pdf(1.0), 0.60653065971263342360, 1e-15));
+/// # Ok::<(), ChiError>(())
 /// ```
 #[derive(Copy, Clone, PartialEq, Debug)]
 pub struct Chi {
@@ -77,10 +78,12 @@ impl Chi {
     /// # Examples
     ///
     /// ```
-    /// use statrs::distribution::Chi;
+    /// use statrs::distribution::{Chi, ChiError};
     ///
-    /// let n = Chi::new(2.0).unwrap();
+    ///
+    /// let n = Chi::new(2.0)?;
     /// assert_eq!(n.freedom(), 2.0);
+    /// # Ok::<(), ChiError>(())
     /// ```
     pub fn freedom(&self) -> f64 {
         self.freedom

src/distribution/chi_squared.rs
@@ -11,13 +11,14 @@ use std::f64;
 /// # Examples
 ///
 /// ```
-/// use statrs::distribution::{ChiSquared, Continuous};
-/// use statrs::statistics::Distribution;
+/// use statrs::distribution::{ChiSquared, Continuous, GammaError};
+/// use statrs::statistics::*;
 /// use statrs::prec;
 ///
-/// let n = ChiSquared::new(3.0).unwrap();
-/// assert_eq!(n.mean().unwrap(), 3.0);
+/// let n = ChiSquared::new(3.0)?;
+/// assert_eq!(n.mean(), 3.0);
 /// assert!(prec::almost_eq(n.pdf(4.0), 0.107981933026376103901, 1e-15));
+/// # Ok::<(), GammaError>(())
 /// ```
 #[derive(Copy, Clone, PartialEq, Debug)]
 pub struct ChiSquared {
@@ -56,10 +57,11 @@ impl ChiSquared {
     /// # Examples
     ///
     /// ```
-    /// use statrs::distribution::ChiSquared;
+    /// use statrs::distribution::{ChiSquared, GammaError};
     ///
-    /// let n = ChiSquared::new(3.0).unwrap();
+    /// let n = ChiSquared::new(3.0)?;
     /// assert_eq!(n.freedom(), 3.0);
+    /// # Ok::<(), GammaError>(())
     /// ```
     pub fn freedom(&self) -> f64 {
         self.freedom
@@ -70,10 +72,11 @@ impl ChiSquared {
     /// # Examples
     ///
     /// ```
-    /// use statrs::distribution::ChiSquared;
+    /// use statrs::distribution::{ChiSquared, GammaError};
     ///
-    /// let n = ChiSquared::new(3.0).unwrap();
+    /// let n = ChiSquared::new(3.0)?;
     /// assert_eq!(n.shape(), 3.0 / 2.0);
+    /// # Ok::<(), GammaError>(())
     /// ```
     pub fn shape(&self) -> f64 {
         self.g.shape()
@@ -84,10 +87,11 @@ impl ChiSquared {
     /// # Examples
     ///
     /// ```
-    /// use statrs::distribution::ChiSquared;
+    /// use statrs::distribution::{ChiSquared, GammaError};
     ///
-    /// let n = ChiSquared::new(3.0).unwrap();
+    /// let n = ChiSquared::new(3.0)?;
     /// assert_eq!(n.rate(), 0.5);
+    /// # Ok::<(), GammaError>(())
     /// ```
     pub fn rate(&self) -> f64 {
         self.g.rate()

commit c8ed456c8697897e7bc44d6f4fc4c92d5b09a01e

fix: variance implementation for slices
bug introduced in a3f78fe554f18715ccc4ea906ff537a32dff1ad0

src/statistics/slice_statistics.rs
@@ -381,18 +381,29 @@ impl<D: AsMut<[f64]> + AsRef<[f64]>> StandardizedMoment<f64> for Data<D> {
         // TODO: rewrite iterator statistics and rely on them instead
         let mut iter = self.0.as_ref().iter().enumerate();
 
-        let mut sum = *iter.next()?.1;
-        let mut variance = 0.0;
+        // init single sample (mean=sample and M_2=0)
+        let mut n: f64 = 1.;
+        let mut mu = *iter.next()?.1;
+        let mut m2 = 0.0;
 
         for (i, &x) in iter {
             if x.is_nan() {
+                // fail early
                 return Some(f64::NAN);
             }
-            let diff = (i + 1) as f64 * x - sum;
-            sum += x;
-            variance += diff * diff / (i * (i + 1)) as f64
+            n = (i + 1) as f64;
+            let delta = x - mu;
+            mu += delta / n;
+            m2 += delta * (x - mu);
         }
-        Some(variance / (self.0.as_ref().len() - 1) as f64)
+
+        if n == 1. {
+            // no sample variance for a single sample
+            None
+        } else {
+            // normalize by bessel
+            Some(m2 / (n - 1.))
+        }
     }
 
     fn skewness(&self) -> Self::Skew {